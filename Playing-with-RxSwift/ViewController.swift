d226f47d8f6725a4e49b3bf0145e1cba7474ccaf
Roberto Gómez Muñoz <robertgomez45@gmail.com>
2017-01-07 15:43:15 +0100

    Create project

---
+++ Playing-with-RxSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Playing-with-RxSwift
+//
+//  Created by Roberto Gomez Muñoz on 7/1/17.
+//  Copyright © 2017 Roberto Gómez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
