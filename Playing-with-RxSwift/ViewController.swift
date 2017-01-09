e7cbb070d27f738f4c1a0865f8825e31a9c2adfa
Roberto Gómez Muñoz <robertgomez45@gmail.com>
2017-01-09 11:35:24 +0100

    SearchBar example with RxSwift

--- Playing-with-RxSwift/ViewController.swift
+++ Playing-with-RxSwift/ViewController.swift
@@ -7,17 +7,59 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    let allSpainProvinces = ["A Coruña", "Álava", "Albacete", "Alicante", "Almería", "Asturias", "Ávila", "Badajoz", "Baleares","Barcelona", "Burgos", "Cáceres", "Cádiz", "Cantabria","Castellón", "Ciudad Real", "Córdoba", "Cuenca", "Girona", "Granada", "Guadalajara", "Gipuzkoa", "Huelva", "Huesca", "Jaén", "La Rioja", "Las Palmas", "León", "Lérida", "Lugo", "Madrid", "Málaga", "Murcia", "Navarra", "Orense", "Palencia", "Pontevedra", "Salamanca", "Segovia", "Sevilla", "Soria", "Tarragona", "Santa Cruz de Tenerife", "Teruel", "Toledo", "Valencia", "Valladolid", "Vizcaya", "Zamora", "Zaragoza"]
+    
+    let disposeBag = DisposeBag()
+    
+    var shownProvinces = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        
+        searchBar
+            .rx.text
+            .throttle(0.5, scheduler: MainScheduler.instance) // Wait 0.5 for changes.
+            .filter {
+                ($0?.characters.count)! > 0
+            }
+            .subscribe(onNext: { (text) in
+                
+                self.shownProvinces = self.allSpainProvinces.filter { $0.hasPrefix(text!) }
+                
+                self.tableView.reloadData()
+                
+            }, onError: { (error: Error) in
+                print("Error: \(error)")
+            })
+            .addDisposableTo(disposeBag)
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shownProvinces.count
+    }
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cityPrototypeCell")
+        cell.textLabel?.text = shownProvinces[indexPath.row]
+        
+        return cell
     }
 
 
